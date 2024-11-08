fb08cf6f2615675ec039a57db7a74a2ed1007c50
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-08 21:22:11 +0530

    modules in node

---
+++ node js/cybrom.js
@@ -0,0 +1,7 @@
+exports.Mycollege=()=>{
+    return "<h3>We are Cybrom students </h3>";
+}
+exports.myfees=()=>{
+    return "<h3>My fees is 56000</h3>";
+}
+
--- node js/index.js
+++ node js/index.js
@@ -1,6 +1,9 @@
+const Myclg=require("./cybrom.js");
 const http=require("http");
 http.createServer((req,res)=>{
 //res.writeHead(200,{'Content-Type':'text/html'}); 
 res.write("<h1> I am MERN Stack Developer</h1>")
+res.write(Myclg.Mycollege());
+res.write(Myclg.myfees());
 res.end("<h3>this is end</h3>");
 }).listen(8000);
