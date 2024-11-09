c03bb08520310375e0a7e933e640dcd9821c41f5
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-09 14:31:01 +0530

    appendFile in fs

--- node js/9nov/index.js
+++ node js/9nov/index.js
@@ -1,13 +1,14 @@
 //read file
-// const http=require("http");
-// const fs=require("fs");
-// http.createServer((req,res)=>{
-// fs.readFile("pawan.txt",(err,data)=>{
-//     res.write(data);
-//     res.end();
-// })
-// }).listen(5000); 
-
+/*
+const http=require("http");
+const fs=require("fs");
+http.createServer((req,res)=>{
+fs.readFile("pawan.txt",(err,data)=>{
+    res.write(data);
+    res.end();
+})
+}).listen(5000); 
+*/
 
 //appendFile
 const fs=require("fs");
