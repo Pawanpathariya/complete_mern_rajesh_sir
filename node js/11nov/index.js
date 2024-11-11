13d816dc36e74c503354a2011a0a8cb799637444
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-11 13:54:22 +0530

    writeFile()

--- node js/11nov/index.js
+++ node js/11nov/index.js
@@ -1,6 +1,15 @@
+/*
 //open()
 const fs=require("fs");
 fs.open("pawam.txt" , "w" , (err)=>{
+    if(err) throw err;
+    console.log("succesfully file created");
+});
+*/
+
+//writeFile()
+const fs=require("fs");
+fs.writeFile("pawam.txt" , "welcome to bhopal" , (err)=>{
     if(err) throw err;
     console.log("succesfully file created");
 });
