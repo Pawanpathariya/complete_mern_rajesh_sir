bee869a43d12228ff76417832472733763beba45
Pawanpathariya <pawanpathariys@gmail.com>
2024-11-11 14:03:04 +0530

    unlink() deleted

--- node js/11nov/index.js
+++ node js/11nov/index.js
@@ -7,9 +7,18 @@ fs.open("pawam.txt" , "w" , (err)=>{
 });
 */
 
+/*
 //writeFile()
 const fs=require("fs");
-fs.writeFile("pawam.txt" , "welcome to bhopal" , (err)=>{
+fs.writeFile("pawam.txt3" , "welcome to bhopal" , (err)=>{
     if(err) throw err;
     console.log("succesfully file created");
 });
+*/
+
+//unlink() delete file 
+const fs=require("fs");
+fs.unlink("pawam.txt",(err)=>{
+    if(err) throw err;
+    console.log("deleted");
+})
